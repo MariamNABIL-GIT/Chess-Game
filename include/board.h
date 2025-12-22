b375892733e553d452fdcbcc5e54e125a6e59f84
مريم نبيل رشدي سرابامون فرج الله <es.mariam.nabil2024@alexu.edu.eg>
2025-12-22 19:48:22 +0200

    Update board.h

--- include/board.h
+++ include/board.h
@@ -2,5 +2,5 @@
 #define BOARD_H
 #include "gamestate.h"
 void initialize_board(char board[8][8]);
-void display_board(char board[8][8],GameState state);
+void display_board(char board[8][8],GameState *state);
 #endif
